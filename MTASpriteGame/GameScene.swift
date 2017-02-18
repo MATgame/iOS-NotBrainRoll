01f99c86305477049d479707ea44f24a0f664cae
Tommy Minwoo Nam <tom.minwoo.nam@gmail.com>
2017-02-17 17:41:29 -0800

    added organ spawning functionality but need to make it dragable again.

--- MTASpriteGame/GameScene.swift
+++ MTASpriteGame/GameScene.swift
@@ -15,11 +15,38 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     var isFingerOnOrgan = false
     var touchLocation = CGPoint()
+    var organDiction = Dictionary<String, SKSpriteNode>()
+    var organList = ["brain", "kidney", "heart", "liver", "lung"]
+    let sceneEdgeGroup : UInt32 = 0b101
+    let organGroup : UInt32 = 0b001
+    let liverShape = SKShapeNode()
     
     override func didMove(to view: SKView) {
-        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        borderBody.friction = 0
+        let borderBody = SKPhysicsBody(edgeLoopFrom: CGRect(x: 0, y: 0, width: size.width, height: 1))
         self.physicsBody = borderBody
+        self.physicsBody?.categoryBitMask = sceneEdgeGroup
+        self.physicsBody?.friction = 0.7
+        
+        let wait = SKAction.wait(forDuration: 3, withRange: 2)
+        let spawn = SKAction.run {
+            let random = arc4random_uniform(5)
+            let newOrgan = SKSpriteNode(imageNamed: self.organList[Int(random)])
+            
+            newOrgan.position = CGPoint(x: 0, y: self.size.height/2)
+            newOrgan.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+            newOrgan.physicsBody?.categoryBitMask = self.organGroup
+            newOrgan.physicsBody?.friction = 8
+            newOrgan.physicsBody?.angularDamping = 10
+            newOrgan.physicsBody?.restitution = 0.3
+            newOrgan.physicsBody?.velocity = CGVector(dx: 200, dy: 500)
+            newOrgan.physicsBody?.affectedByGravity = true
+            
+            self.addChild(newOrgan)
+            
+        }
+        
+        let sequence = SKAction.sequence([wait, spawn])
+        self.run(SKAction.repeatForever(sequence))
     }
     
     
@@ -37,12 +64,19 @@ class GameScene: SKScene {
         touchLocation = touch!.location(in:self)
         
         if let body = physicsWorld.body(at: touchLocation) {
+            if organDiction[body.node!.name!] != nil {
+                print("Began touch on organ")
+                isFingerOnOrgan = true
+            }
+            
+        }
+        /*if let body = physicsWorld.body(at: touchLocation) {
             if body.node!.name == "organ" {
                     print("Began touch on organ")
                 isFingerOnOrgan = true
             }
         
-        }
+        }*/
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
